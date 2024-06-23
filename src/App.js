d139b3a83a0335ee326be2e64eb033353f0e1458
rowdotk <koklanrow@gmail.com>
2024-06-23 22:21:24 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./styles/Board.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Board from "./components/Board.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Board />
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/tic-tac-toe" element={<Board />} />
+        <Route path="*" element={<Navigate to="/tic-tac-toe" />} />
+      </Routes>
+    </Router>
   );
 }
 
